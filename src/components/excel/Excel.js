610eda1d34f6ac115a95b7d5cc2c582a0723c6a1
WebDev TVlean <tvleanjohnny@gmail.com>
2020-09-18 18:00:32 +0300

    add component

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,19 @@ export class Excel {
     this.$el = document.querySelector(selector)
     this.components = options.components || []
   }
+
+  getRoot() {
+    const $root = document.createElement('div')
+
+    this.components.forEach( Component => {
+      const component = new Component()
+      console.log(component)
+    });
+
+    return $root
+  }
+
+  render() {
+    this.$el.append(this.getRoot())
+  }
 }
